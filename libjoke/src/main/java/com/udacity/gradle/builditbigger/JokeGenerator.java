4c700c0fa3fd82417a8dd94ed3063f2b73e1617d
Davi Caetano <davicaetano@gmail.com>
2015-10-10 10:45:59 -0700

    Commit 3: fixing java lib

--- libjoke/src/main/java/com/udacity/gradle/builditbigger/JokeGenerator.java
+++ libjoke/src/main/java/com/udacity/gradle/builditbigger/JokeGenerator.java
@@ -9,6 +9,6 @@ public class JokeGenerator
         String[] jokes = {"Software is like sex: It’s better when it’s free.",
                 "Q: Why did the ants dance on the jam jar?\nA: The lid said, \"Twist to open.\"",
                 "Q: how many programmers does it take to change a light bulb?\nA: none, that’s a hardware problem"};
-        return jokes[random.nextInt(3)];
+        return jokes[random.nextInt(jokes.length)];
     }
 }
